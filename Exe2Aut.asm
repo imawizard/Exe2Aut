245c365d66c9191e8fdea8dc1711398f003c950e
imawizard <imawizard@outlook.de>
2022-02-16 14:00:39 +0100

    Add a crash dialog

--- Exe2Aut.asm
+++ Exe2Aut.asm
@@ -7,6 +7,12 @@ include 'Exe2Aut.inc'
 section '.code' code readable executable
 
   start:
+	push	ExceptionFilter
+	call	[SetUnhandledExceptionFilter]
+	push	_startup
+	push	state
+	call	[strcpy]
+	add	esp,8
 	push	_mutex
 	push	0
 	push	0
@@ -194,6 +200,10 @@ proc DialogProc hwnd,msg,wparam,lparam
     .wm_initdialog:
 	mov	eax,[hwnd]
 	mov	[main_hwnd],eax
+	push	_idle
+	push	state
+	call	[strcpy]
+	add	esp,8
 	push	0
 	push	[hwnd]
 	call	[GetSystemMenu]
@@ -552,6 +562,10 @@ endp
 	mov	ecx,NO_3264BIT
 	jmp	.fin
     .start:
+	push	_decompiling
+	push	state
+	call	[strcpy]
+	add	esp,8
 	mov	[_si.cb],sizeof.STARTUPINFO
 	xor	eax,eax
 	push	_pi
@@ -799,6 +813,12 @@ endp
 	xor	edx,edx
     .fin:
 	xchg	eax,ecx
+	push	eax edx
+	push	_idle
+	push	state
+	call	[strcpy]
+	add	esp,8
+	pop	edx eax
 	pop	edi esi ebx
 	retn
 
@@ -1059,6 +1079,171 @@ proc AboutProc hwnd,msg,wparam,lparam
 	ret
 endp
 
+proc CrashProc hwnd,msg,wparam,lparam
+  local buf[200h]:BYTE,buf2[33]:BYTE
+	cmp	[msg],WM_INITDIALOG
+	je	.wm_initdialog
+	cmp	[msg],WM_CTLCOLORSTATIC
+	je	.wm_ctlcolorstatic
+	cmp	[msg],WM_COMMAND
+	je	.wm_command
+	cmp	[msg],WM_CLOSE
+	je	.wm_close
+	xor	eax,eax
+	jmp	.fin
+    .wm_initdialog:
+	push	edi
+	push	0
+	call	[GetModuleHandle]
+	push	0
+	push	16
+	push	16
+	push	IMAGE_ICON
+	push	IDI_ICON2
+	push	eax
+	call	[LoadImage]
+	push	eax
+	push	0
+	push	WM_SETICON
+	push	[hwnd]
+	call	[SendMessage]
+	push	_courier
+	push	DEFAULT_PITCH+FF_MODERN
+	push	DEFAULT_QUALITY
+	push	CLIP_DEFAULT_PRECIS
+	push	OUT_DEFAULT_PRECIS
+	push	ANSI_CHARSET
+	push	0
+	push	0
+	push	0
+	push	FW_NORMAL
+	push	0
+	push	0
+	push	0
+	push	-11
+	call	[CreateFont]
+	push	0
+	push	eax
+	push	WM_SETFONT
+	push	IDC_CRASH
+	push	[hwnd]
+	call	[SendDlgItemMessage]
+	mov	edi,[lparam]
+	mov	edi,[edi+EXCEPTION_POINTERS.ContextRecord]
+	push	[edi+CONTEXT.Eip]
+	call	alloc_base
+	push	MAX_PATH
+	push	path
+	push	eax
+	call	[GetModuleFileName]
+	push	path
+	call	[PathStripPath]
+	push	path
+	call	[strlen]
+	push	edi
+	mov	ecx,32
+	lea	edi,[buf2]
+	sub	ecx,eax
+	js	.no_padding
+	mov	al,' '
+	rep	stosb
+      .no_padding:
+	mov	byte [edi],0
+	pop	edi
+	lea	eax,[buf2]
+	push	path
+	push	eax
+	push	[edi+CONTEXT.EFlags]
+	push	[edi+CONTEXT.Eip]
+	push	[edi+CONTEXT.Edi]
+	push	[edi+CONTEXT.Esi]
+	push	[edi+CONTEXT.Ebp]
+	push	[edi+CONTEXT.Esp]
+	push	[edi+CONTEXT.Ebx]
+	push	[edi+CONTEXT.Edx]
+	push	[edi+CONTEXT.Ecx]
+	push	[edi+CONTEXT.Eax]
+	push	state
+	lea	eax,[buf]
+	push	_crash
+	push	eax
+	call	[wsprintf]
+	add	esp,4*15
+	push	_crash
+	push	IDC_CRASH
+	push	[hwnd]
+	call	[SetDlgItemText]
+	lea	eax,[buf]
+	push	eax
+	push	IDC_CRASH
+	push	[hwnd]
+	call	[SetDlgItemText]
+	push	0
+	push	0
+	push	EM_SETSEL
+	push	IDC_CRASH
+	push	[hwnd]
+	call	[SendDlgItemMessage]
+	push	IDC_EXIT
+	push	[hwnd]
+	call	[GetDlgItem]
+	push	eax
+	call	[SetFocus]
+	pop	edi
+	xor	eax,eax
+	jmp	.fin
+    .wm_ctlcolorstatic:
+	push	000FF00h
+	push	[wparam]
+	call	[SetTextColor]
+	push	TRANSPARENT
+	push	[wparam]
+	call	[SetBkMode]
+	push	BLACK_BRUSH
+	call	[GetStockObject]
+	jmp	.fin
+    .wm_command:
+	cmp	[wparam],IDCANCEL
+	je	.wm_close
+	cmp	[wparam],BN_CLICKED shl 16+IDC_EXIT
+	jnz	.done
+    .wm_close:
+	push	0
+	push	[hwnd]
+	call	[EndDialog]
+    .done:
+	mov	eax,1
+    .fin:
+	ret
+endp
+
+proc ExceptionFilter ExceptionInfo
+	cmp	[progress_hwnd],0
+	je	.no_progress_hwnd
+	push	0
+	push	0
+	push	WM_CLOSE
+	push	[progress_hwnd]
+	call	[SendMessage]
+    .no_progress_hwnd:
+	cmp	[main_hwnd],0
+	je	.no_main_hwnd
+	push	SW_HIDE
+	push	[main_hwnd]
+	call	[ShowWindow]
+    .no_main_hwnd:
+	push	0
+	call	[GetModuleHandle]
+	push	[ExceptionInfo]
+	push	CrashProc
+	push	0
+	push	IDD_CRASH
+	push	eax
+	call	[DialogBoxParam]
+	mov	eax,1
+	ret
+endp
+
 	include 'misc.inc'
 	include 'deobfuscate.inc'
 	include 'gfx.inc'
@@ -1109,6 +1294,16 @@ section '.data' data readable writeable
   _rename du '-r',0
   _fileinst du '-f',0
   _compiled du '-c',0
+  _crash db 'I''m sorry to tell you that Exe2Aut has crashed :(',13,10
+	 db '(',VERSION,' [%s], built ',DATE,')',13,10,13,10
+	 db 'EAX = %08Xh  ECX = %08Xh  EDX = %08Xh',13,10
+	 db 'EBX = %08Xh  ESP = %08Xh  EBP = %08Xh',13,10
+	 db 'ESI = %08Xh  EDI = %08Xh  EIP = %08Xh',13,10
+	 db 'Flags:%08Xh%s(%s)',0
+  _startup db 'startup',0
+  _idle db 'idle',0
+  _decompiling db 'decompiling',0
+  _deobfuscating db 'deobfuscating',0
 
   s_deobfusc dd BST_CHECKED
   s_rename dd 0
@@ -1120,6 +1315,8 @@ section '.data' data readable writeable
   misc_idata
   deobfus_idata
   gfx_idata
+
+  state rb 10h
 
   path rb 256
   pathdll rb 256
@@ -1151,7 +1348,8 @@ section '.idata' import data readable
 	  user32,'USER32.DLL',\
 	  gdi32,'GDI32.DLL',\
 	  shell32,'SHELL32.DLL',\
-	  msvcrt,'MSVCRT.DLL'
+	  msvcrt,'MSVCRT.DLL',\
+	  shlwapi,'SHLWAPI.DLL'
 
   include 'api\kernel32.inc'
   include 'api\user32.inc'
@@ -1179,6 +1377,9 @@ section '.idata' import data readable
 	 wcsncmp,'wcsncmp',\
 	 wtoi,'_wtoi'
 
+  import shlwapi,\
+	 PathStripPath,'PathStripPathA'
+
 section '.rsrc' resource data readable
 
   IDI_ICON1    = 1
@@ -1190,17 +1391,20 @@ section '.rsrc' resource data readable
   IDD_DEOBFU   = 101
   IDD_PROGRESS = 102
   IDD_ABOUT    = 103
-  IDC_RESULT   = 104
-  IDC_SAVE     = 105
-  IDC_DEOBFUSC = 106
-  IDC_SYMBOLS  = 107
-  IDC_FILEINST = 108
-  IDC_COMPILED = 109
-  IDC_PROGRESS = 110
-  IDM_ARMDB    = 111
-  IDM_NOFILES  = 112
-  IDM_DEOBFU   = 113
-  IDM_ABOUT    = 114
+  IDD_CRASH    = 104
+  IDC_RESULT   = 1000
+  IDC_SAVE     = 1001
+  IDC_DEOBFUSC = 1002
+  IDC_SYMBOLS  = 1003
+  IDC_FILEINST = 1004
+  IDC_COMPILED = 1005
+  IDC_PROGRESS = 1006
+  IDC_CRASH    = 1007
+  IDC_EXIT     = 1008
+  IDM_ARMDB    = 2000
+  IDM_NOFILES  = 2001
+  IDM_DEOBFU   = 2002
+  IDM_ABOUT    = 2003
 
   directory RT_ICON,icons,\
 	    RT_GROUP_ICON,group_icons,\
@@ -1224,7 +1428,8 @@ section '.rsrc' resource data readable
 	   IDD_MAIN,LANG_ENGLISH+SUBLANG_DEFAULT,main_dialog,\
 	   IDD_DEOBFU,LANG_ENGLISH+SUBLANG_DEFAULT,deobfu_dialog,\
 	   IDD_PROGRESS,LANG_ENGLISH+SUBLANG_DEFAULT,progress_dialog,\
-	   IDD_ABOUT,LANG_ENGLISH+SUBLANG_DEFAULT,about_dialog
+	   IDD_ABOUT,LANG_ENGLISH+SUBLANG_DEFAULT,about_dialog,\
+	   IDD_CRASH,LANG_ENGLISH+SUBLANG_DEFAULT,crash_dialog
 
   resource manifests,\
 	   1,LANG_ENGLISH+SUBLANG_DEFAULT,manifest
@@ -1286,4 +1491,9 @@ section '.rsrc' resource data readable
   enddialog
 
   dialog about_dialog,'',0,0,176,100,WS_POPUP
+  enddialog
+
+  dialog crash_dialog,WINDOW_TITLE,0,0,230,80,WS_POPUP+WS_CAPTION+WS_MINIMIZEBOX+DS_CENTER
+    dialogitem 'edit','',IDC_CRASH,0,0,230,62,WS_VISIBLE+ES_MULTILINE+ES_READONLY
+    dialogitem 'button','Exit',IDC_EXIT,95,66,40,12,WS_VISIBLE
   enddialog
